62c3f6e8dcbce7a1aab7dd6cb306cb6350719be8
Dan Hollingsworth <dan@machinepublishers.com>
2022-05-04 10:33:29 -0400

    Migrate build to use Gradle Catalogs (#172)
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

---
+++ problem-json/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("io.micronaut.build.internal.module")
+}
+
+dependencies {
+    annotationProcessor(mn.micronaut.serde.processor)
+    api(libs.managed.zalando.problem)
+    implementation(mn.micronaut.serde.api)
+    implementation(mn.micronaut.validation)
+    implementation(mn.micronaut.http.server)
+
+    testImplementation(mn.micronaut.http.server.netty)
+    testImplementation(mn.micronaut.http.client)
+    testAnnotationProcessor(mn.micronaut.inject.java)
+}
