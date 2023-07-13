e10e9f20e25b2d4122fd6f79823667187c4c4705
sdelamo <sdelamo@users.noreply.github.com>
2023-07-13 16:18:55 +0000

    Deploying to gh-pages - 16:18:55

---
+++ 3.0.1/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.problem"},{"l":"io.micronaut.problem.conf"},{"l":"io.micronaut.problem.serde"},{"l":"io.micronaut.problem.violations"}];updateSearchResults();
